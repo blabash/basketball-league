57cfdcb75305b9e02195297b30ad13e9b16b78db
brett <brett.labash@gmail.com>
2019-12-19 20:59:54 -0800

    Turn routes into lazy loaded components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Home';
-import Players from './Players';
-import Teams from './Teams';
 import Navbar from './Navbar';
-import TeamPage from './TeamPage';
-import Articles from './Articles';
+import Loading from './Loading';
+
+const LazyHome = React.lazy(() => import('./Home'));
+const LazyTeams = React.lazy(() => import('./Teams'));
+const LazyPlayers = React.lazy(() => import('./Players'));
+const LazyTeamPage = React.lazy(() => import('./TeamPage'));
+const LazyArticles = React.lazy(() => import('./Articles'));
 
 const App: React.FC = () => {
   return (
     <Router>
       <div>
         <Route component={Navbar} />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/teams' component={Teams} />
-          <Route path='/players' component={Players} />
-          <Route path='/:teamId' exact component={TeamPage} />
-          <Route path='/:teamId/articles' component={Articles} />
-          <Route render={() => <h1 className='text-center pt-4'>404.</h1>} />
-        </Switch>
+        <React.Suspense fallback={null}>
+          <Switch>
+            <Route path='/' exact component={LazyHome} />
+            <Route path='/teams' component={LazyTeams} />
+            <Route path='/players' component={LazyPlayers} />
+            <Route path='/:teamId' exact component={LazyTeamPage} />
+            <Route path='/:teamId/articles' component={LazyArticles} />
+            <Route render={() => <h1 className='text-center pt-4'>404.</h1>} />
+          </Switch>
+        </React.Suspense>
       </div>
     </Router>
   );
